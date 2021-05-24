fd768f512cc7cb52b9d7aa3454abfcbbb812811f
jtuu <joona.tuukkanen@gmail.com>
2021-05-24 21:17:37 +0300

    Moved wine-build.sh to project root

--- wine-build.sh
+++ wine-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run --rm -v "$(readlink -f .)":/bbpp -w /bbpp msvc:latest bash -c \
-"/opt/msvc/bin/x86/cl /nologo /LD /EHsc /I. *.cpp $* /link /OUT:bbpp.dll /subsystem:console User32.lib;
+"/opt/msvc/bin/x86/cl /nologo /LD /EHsc /I. Blue\ Burst\ Patch\ Project/*.cpp $* /link /OUT:bbpp.dll /subsystem:console User32.lib;
 chown $(id -u $USER):$(id -g $USER) bbpp.dll"
