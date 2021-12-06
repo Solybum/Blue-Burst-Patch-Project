5df76920edd7be88da5d094d6d1d1a723d48d755
jtuu <joona.tuukkanen@gmail.com>
2021-12-06 04:27:49 +0200

    Use different enum for each BP table's indices because they differ

--- Blue Burst Patch Project/battleparam.h
+++ Blue Burst Patch Project/battleparam.h
@@ -5,7 +5,7 @@
 
 namespace BattleParam
 {
-    enum class BPIndex : uint8_t
+    enum class BPStatsIndex : uint8_t
     {
         ep1_Mothmant__ep2_Mothmant__ep4_Boota = 0x0,
         ep1_Monest__ep2_Monest__ep4_ZeBoota = 0x1,
@@ -88,14 +88,286 @@ namespace BattleParam
         ep1_LaDimenian__ep2_LaDimenian__ep4_None = 0x54,
         ep1_SoDimenian__ep2_SoDimenian__ep4_None = 0x55,
 
-        /// Greatest index that is used by vanilla
-        END_INDEX = ep1_SoDimenian__ep2_SoDimenian__ep4_None,
-
-        /// The vanilla bp tables each have 96 entries
-        MAX_INDEX = 95,
-
-        INVALID = 0xff
+        None = 0xff
     };
+
+    enum class BPAttacksIndex : uint8_t
+    {
+        ep1_Mothmant__ep2_Mothmant__ep4_Boota = 0x0,
+        ep1_Monest__ep2_Monest__ep4_ZeBoota = 0x1,
+        ep1_SavageWolf__ep2_SavageWolf__ep4_ZeBootaAttack1 = 0x2,
+        ep1_BarbarousWolf__ep2_BarbarousWolf__ep4_BaBoota = 0x3,
+        ep1_PoisonLily__ep2_PoisonLily__ep4_BaBootaAttack1 = 0x4,
+        ep1_NarLily__ep2_NarLily__ep4_SandRappyCrater = 0x5,
+        ep1_SinowBeat__ep2_SinowBerill__ep4_DelRappyCrater = 0x6,
+        ep1_Canadine__ep2_Gee__ep4_ZuCrater = 0x7,
+        ep1_CanadineRing__ep2_None__ep4_PazuzuCrater = 0x8,
+        ep1_Canane__ep2_None__ep4_None = 0x9,
+        ep1_ChaosSorcerer__ep2_ChaosSorcerer__ep4_Astark = 0xa,
+        ep1_BeeR__ep2_BeeR__ep4_AstarkAttack1 = 0xb,
+        ep1_BeeL__ep2_BeeL__ep4_AstarkAttack2 = 0xc,
+        ep1_ChaosBringer__ep2_Delbiter__ep4_SatelliteLizardCrater = 0xd,
+        ep1_DarkBelra__ep2_DarkBelra__ep4_YowieCrater = 0xe,
+        ep1_DeRolLe__ep2_BarbaRay__ep4_Dorphon = 0xf,
+        ep1_DeRolLeShell__ep2_PigRay__ep4_DorphonEclair = 0x10,
+        ep1_DeRolLeMine__ep2_UlRay__ep4_Goran = 0x11,
+        ep1_Dragon__ep2_GolDragon__ep4_PyroGoran = 0x12,
+        ep1_BelraFist__ep2_SinowSpigell__ep4_GoranDetonator = 0x13,
+        ep1_None__ep2_None__ep4_GoranAttack1 = 0x14,
+        ep1_None__ep2_None__ep4_PyroGoranAttack1 = 0x15,
+        ep1_None__ep2_None__ep4_GoranDetonatorAttack1 = 0x16,
+        ep1_None__ep2_None__ep4_SandRappyDesert = 0x17,
+        ep1_RagRappy__ep2_RagRappy__ep4_DelRappyDesert = 0x18,
+        ep1_AlRappy__ep2_LoveRappy__ep4_MerissaA = 0x19,
+        ep1_NanoDragon__ep2_GiGue__ep4_MerissaAA = 0x1a,
+        ep1_Dubchic__ep2_Dubchic__ep4_ZuDesert = 0x1b,
+        ep1_Gillchic__ep2_Gillchic__ep4_PazuzuDesert = 0x1c,
+        ep1_Garanz__ep2_Garanz__ep4_SatelliteLizardDesert = 0x1d,
+        ep1_DarkGunner__ep2_GalGryphon__ep4_YowieDesert = 0x1e,
+        ep1_Bulclaw__ep2_None__ep4_Girtablulu = 0x1f,
+        ep1_Claw__ep2_None__ep4_SaintMilionPhase1 = 0x20,
+        ep1_VolOptForm1__ep2_None__ep4_SpinnerSaintMilion1 = 0x21,
+        ep1_VolOptPillar__ep2_None__ep4_SaintMilionPhase2 = 0x22,
+        ep1_VolOptMonitor__ep2_Epsilon__ep4_SpinnerSaintMilion2 = 0x23,
+        ep1_VolOptSpire__ep2_Epsigard__ep4_ShambertinPhase1 = 0x24,
+        ep1_VolOptForm2__ep2_DelLily__ep4_SpinnerShambertin1 = 0x25,
+        ep1_VolOptPrison__ep2_IllGill__ep4_ShambertinPhase2 = 0x26,
+        ep1_None__ep2_IllGillAttack1__ep4_SpinnerShambertin2 = 0x27,
+        ep1_None__ep2_IllGillAttack2__ep4_KondrieuPhase1 = 0x28,
+        ep1_None__ep2_IllGillAttack3__ep4_SpinnerKondrieu1 = 0x29,
+        ep1_None__ep2_None__ep4_KondrieuPhase2 = 0x2a,
+        ep1_None__ep2_OlgaFlowForm1__ep4_SpinnerKondrieu2 = 0x2b,
+        ep1_None__ep2_OlgaFlowForm2__ep4_None = 0x2c,
+        ep1_None__ep2_Gael__ep4_None = 0x2d,
+        ep1_None__ep2_Giel__ep4_None = 0x2e,
+        ep1_PofuillySlime__ep2_Deldepth__ep4_None = 0x30,
+        ep1_PanArms__ep2_PanArms__ep4_None = 0x31,
+        ep1_Hidoom__ep2_Hidoom__ep4_None = 0x32,
+        ep1_Migium__ep2_Migium__ep4_None = 0x33,
+        ep1_PouillySlime__ep2_None__ep4_None = 0x34,
+        ep1_Darvant__ep2_None__ep4_None = 0x35,
+        ep1_DarkFalzForm1__ep2_None__ep4_None = 0x36,
+        ep1_DarkFalzForm2__ep2_None__ep4_None = 0x37,
+        ep1_DarkFalzForm3__ep2_None__ep4_None = 0x38,
+        ep1_DarvantFalz__ep2_None__ep4_None = 0x39,
+        ep1_None__ep2_Mericarol__ep4_None = 0x3a,
+        ep1_None__ep2_UlGibbon__ep4_None = 0x3b,
+        ep1_None__ep2_ZolGibbon__ep4_None = 0x3c,
+        ep1_None__ep2_Gibbles__ep4_None = 0x3d,
+        ep1_None__ep2_GibblesAttack1__ep4_None = 0x3e,
+        ep1_None__ep2_GibblesAttack2__ep4_None = 0x3f,
+        ep1_None__ep2_Morfos__ep4_None = 0x40,
+        ep1_None__ep2_Recobox__ep4_None = 0x41,
+        ep1_None__ep2_Recon__ep4_None = 0x42,
+        ep1_None__ep2_SinowZoa__ep4_None = 0x43,
+        ep1_None__ep2_SinowZele__ep4_None = 0x44,
+        ep1_None__ep2_Merikle__ep4_None = 0x45,
+        ep1_None__ep2_Mericus__ep4_None = 0x46,
+        ep1_SinowGold__ep2_SinowSpigell__ep4_None = 0x47,
+        ep1_Hildebear__ep2_Hildebear__ep4_None = 0x48,
+        ep1_HildebearAttack1__ep2_HildebearAttack1__ep4_None = 0x49,
+        ep1_HildebearAttack2__ep2_HildebearAttack2__ep4_None = 0x4a,
+        ep1_Hildeblue__ep2_Hildeblue__ep4_None = 0x4b,
+        ep1_HildeblueAttack1__ep2_HildeblueAttack1__ep4_None = 0x4c,
+        ep1_HildeblueAttack2__ep2_HildeblueAttack2__ep4_None = 0x4d,
+        ep1_Booma__ep2_Merillia__ep4_None = 0x4e,
+        ep1_Gobooma__ep2_Meriltas__ep4_None = 0x4f,
+        ep1_Gigobooma__ep2_MorfosAttack1__ep4_None = 0x50,
+        ep1_GrassAssassin__ep2_GrassAssassin__ep4_None = 0x51,
+        ep1_GrassAssassinAttack1__ep2_GrassAssassinAttack1__ep4_None = 0x52,
+        ep1_GrassAssassinAttack2__ep2_GrassAssassinAttack2__ep4_None = 0x53,
+        ep1_EvilShark__ep2_Dolmolm__ep4_None = 0x54,
+        ep1_PalShark__ep2_Dolmdarl__ep4_None = 0x55,
+        ep1_GuilShark__ep2_None__ep4_None = 0x56,
+        ep1_Delsaber__ep2_Delsaber__ep4_None = 0x57,
+        ep1_DelsaberAttack1__ep2_DelsaberAttack1__ep4_None = 0x58,
+        ep1_DelsaberAttack2__ep2_DelsaberAttack2__ep4_None = 0x59,
+        ep1_Dimenian__ep2_Dimenian__ep4_None = 0x5a,
+        ep1_LaDimenian__ep2_LaDimenian__ep4_None = 0x5b,
+        ep1_SoDimenian__ep2_SoDimenian__ep4_None = 0x5c,
+
+        None = 0xff
+    };
+
+    enum class BPResistsIndex : uint8_t
+    {
+        ep1_Mothmant__ep2_Mothmant__ep4_Boota = 0x0,
+        ep1_Monest__ep2_Monest__ep4_ZeBoota = 0x1,
+        ep1_SavageWolf__ep2_SavageWolf__ep4_None = 0x2,
+        ep1_BarbarousWolf__ep2_BarbarousWolf__ep4_BaBoota = 0x3,
+        ep1_PoisonLily__ep2_PoisonLily__ep4_None = 0x4,
+        ep1_NarLily__ep2_NarLily__ep4_SandRappyCrater = 0x5,
+        ep1_SinowBeat__ep2_SinowBerill__ep4_DelRappyCrater = 0x6,
+        ep1_Canadine__ep2_Gee__ep4_ZuCrater = 0x7,
+        ep1_CanadineRing__ep2_None__ep4_PazuzuCrater = 0x8,
+        ep1_Canane__ep2_None__ep4_Astark = 0x9,
+        ep1_ChaosSorcerer__ep2_ChaosSorcerer__ep4_None = 0xa,
+        ep1_BeeR__ep2_BeeR__ep4_None = 0xb,
+        ep1_BeeL__ep2_BeeL__ep4_None = 0xc,
+        ep1_ChaosBringer__ep2_Delbiter__ep4_SatelliteLizardCrater = 0xd,
+        ep1_DarkBelra__ep2_DarkBelra__ep4_YowieCrater = 0xe,
+        ep1_DeRolLe__ep2_BarbaRay__ep4_Dorphon = 0xf,
+        ep1_DeRolLeShell__ep2_PigRay__ep4_DorphonEclair = 0x10,
+        ep1_DeRolLeMine__ep2_UlRay__ep4_Goran = 0x11,
+        ep1_Dragon__ep2_GolDragon__ep4_PyroGoran = 0x12,
+        ep1_SinowGold__ep2_SinowSpigell__ep4_GoranDetonator = 0x13,
+        // Empty slots
+        ep1_None__ep2_None__ep4_SandRappyDesert = 0x17,
+        ep1_RagRappy__ep2_RagRappy__ep4_DelRappyDesert = 0x18,
+        ep1_AlRappy__ep2_LoveRappy__ep4_MerissaA = 0x19,
+        ep1_NanoDragon__ep2_GiGue__ep4_MerissaAA = 0x1a,
+        ep1_Dubchic__ep2_Dubchic__ep4_ZuDesert = 0x1b,
+        ep1_Gillchic__ep2_Gillchic__ep4_PazuzuDesert = 0x1c,
+        ep1_Garanz__ep2_Garanz__ep4_SatelliteLizardDesert = 0x1d,
+        ep1_DarkGunner__ep2_GalGryphon__ep4_YowieDesert = 0x1e,
+        ep1_Bulclaw__ep2_None__ep4_Girtablulu = 0x1f,
+        ep1_Claw__ep2_None__ep4_SaintMilionPhase1 = 0x20,
+        ep1_VolOptForm1__ep2_None__ep4_SpinnerSaintMilion1 = 0x21,
+        ep1_VolOptPillar__ep2_None__ep4_SaintMilionPhase2 = 0x22,
+        ep1_VolOptMonitor__ep2_Epsilon__ep4_SpinnerSaintMilion2 = 0x23,
+        ep1_VolOptSpire__ep2_Epsigard__ep4_ShambertinPhase1 = 0x24,
+        ep1_VolOptForm2__ep2_DelLily__ep4_SpinnerShambertin1 = 0x25,
+        ep1_VolOptPrison__ep2_IllGill__ep4_ShambertinPhase2 = 0x26,
+        ep1_None__ep2_None__ep4_SpinnerShambertin2 = 0x27,
+        ep1_None__ep2_None__ep4_KondrieuPhase1 = 0x28,
+        ep1_None__ep2_None__ep4_SpinnerKondrieu1 = 0x29,
+        ep1_None__ep2_None__ep4_KondrieuPhase2 = 0x2a,
+        ep1_None__ep2_OlgaFlowForm1__ep4_SpinnerKondrieu2 = 0x2b,
+        ep1_None__ep2_OlgaFlowForm2__ep4_None = 0x2c,
+        ep1_None__ep2_Gael__ep4_None = 0x2d,
+        ep1_None__ep2_Giel__ep4_None = 0x2e,
+        ep1_PofuillySlime__ep2_Deldepth__ep4_None = 0x30,
+        ep1_PanArms__ep2_PanArms__ep4_None = 0x31,
+        ep1_Hidoom__ep2_Hidoom__ep4_None = 0x32,
+        ep1_Migium__ep2_Migium__ep4_None = 0x33,
+        ep1_PouillySlime__ep2_None__ep4_None = 0x34,
+        ep1_Darvant__ep2_None__ep4_None = 0x35,
+        ep1_DarkFalzForm1__ep2_None__ep4_None = 0x36,
+        ep1_DarkFalzForm2__ep2_None__ep4_None = 0x37,
+        ep1_DarkFalzForm3__ep2_None__ep4_None = 0x38,
+        ep1_DarvantFalz__ep2_None__ep4_None = 0x39,
+        ep1_None__ep2_Mericarol__ep4_None = 0x3a,
+        ep1_None__ep2_UlGibbon__ep4_None = 0x3b,
+        ep1_None__ep2_ZolGibbon__ep4_None = 0x3c,
+        ep1_None__ep2_Gibbles__ep4_None = 0x3d,
+        ep1_None__ep2_Morfos__ep4_None = 0x40,
+        ep1_None__ep2_Recobox__ep4_None = 0x41,
+        ep1_None__ep2_Recon__ep4_None = 0x42,
+        ep1_None__ep2_SinowZoa__ep4_None = 0x43,
+        ep1_None__ep2_SinowZele__ep4_None = 0x44,
+        ep1_None__ep2_Merikle__ep4_None = 0x45,
+        ep1_None__ep2_Mericus__ep4_None = 0x46,
+        ep1_Hildebear__ep2_Hildebear__ep4_None = 0x48,
+        ep1_Hildeblue__ep2_Hildeblue__ep4_None = 0x49,
+        ep1_Booma__ep2_Merillia__ep4_None = 0x4a,
+        ep1_Gobooma__ep2_Meriltas__ep4_None = 0x4b,
+        ep1_Gigobooma__ep2_None__ep4_None = 0x4c,
+        ep1_GrassAssassin__ep2_GrassAssassin__ep4_None = 0x4d,
+        ep1_EvilShark__ep2_Dolmolm__ep4_None = 0x4e,
+        ep1_PalShark__ep2_Dolmdarl__ep4_None = 0x4f,
+        ep1_GuilShark__ep2_None__ep4_None = 0x50,
+        ep1_Delsaber__ep2_Delsaber__ep4_None = 0x51,
+        ep1_Dimenian__ep2_Dimenian__ep4_None = 0x52,
+        ep1_LaDimenian__ep2_LaDimenian__ep4_None = 0x53,
+        ep1_SoDimenian__ep2_SoDimenian__ep4_None = 0x54,
+
+        None = 0xff
+    };
+
+    enum class BPAnimationsIndex : uint8_t
+    {
+        ep1_Mothmant__ep2_Mothmant__ep4_Boota = 0x0,
+        ep1_Monest__ep2_Monest__ep4_ZeBoota = 0x1,
+        ep1_SavageWolf__ep2_SavageWolf__ep4_None = 0x2,
+        ep1_BarbarousWolf__ep2_BarbarousWolf__ep4_BaBoota = 0x3,
+        ep1_PoisonLily__ep2_PoisonLily__ep4_None = 0x4,
+        ep1_NarLily__ep2_NarLily__ep4_SandRappyCrater = 0x5,
+        ep1_SinowBeat__ep2_SinowBerill__ep4_DelRappyCrater = 0x6,
+        ep1_Canadine__ep2_Gee__ep4_ZuCrater = 0x7,
+        ep1_CanadineRing__ep2_None__ep4_PazuzuCrater = 0x8,
+        ep1_Canane__ep2_None__ep4_Astark = 0x9,
+        ep1_ChaosSorcerer__ep2_ChaosSorcerer__ep4_None = 0xa,
+        ep1_BeeR__ep2_BeeR__ep4_None = 0xb,
+        ep1_BeeL__ep2_BeeL__ep4_None = 0xc,
+        ep1_ChaosBringer__ep2_Delbiter__ep4_SatelliteLizardCrater = 0xd,
+        ep1_DarkBelra__ep2_DarkBelra__ep4_YowieCrater = 0xe,
+        ep1_DeRolLe__ep2_BarbaRay__ep4_Dorphon = 0xf,
+        ep1_SinowGold__ep2_SinowSpigell__ep4_DorphonEclair = 0x10,
+        ep1_DeRolLeMine__ep2_UlRay__ep4_Goran = 0x11,
+        ep1_Dragon__ep2_GolDragon__ep4_PyroGoran = 0x12,
+        ep1_SinowGold__ep2_SinowSpigell__ep4_GoranDetonator = 0x13,
+        // Empty slots
+        ep1_None__ep2_None__ep4_SandRappyDesert = 0x17,
+        ep1_RagRappy__ep2_RagRappy__ep4_DelRappyDesert = 0x18,
+        ep1_AlRappy__ep2_LoveRappy__ep4_MerissaA = 0x19,
+        ep1_NanoDragon__ep2_GiGue__ep4_MerissaAA = 0x1a,
+        ep1_Dubchic__ep2_Dubchic__ep4_ZuDesert = 0x1b,
+        ep1_Gillchic__ep2_Gillchic__ep4_PazuzuDesert = 0x1c,
+        ep1_Garanz__ep2_Garanz__ep4_SatelliteLizardDesert = 0x1d,
+        ep1_DarkGunner__ep2_GalGryphon__ep4_YowieDesert = 0x1e,
+        ep1_Bulclaw__ep2_None__ep4_Girtablulu = 0x1f,
+        ep1_Claw__ep2_None__ep4_SaintMilionPhase1 = 0x20,
+        ep1_VolOptForm1__ep2_None__ep4_SpinnerSaintMilion1 = 0x21,
+        ep1_VolOptPillar__ep2_None__ep4_SaintMilionPhase2 = 0x22,
+        ep1_VolOptMonitor__ep2_Epsilon__ep4_SpinnerSaintMilion2 = 0x23,
+        ep1_VolOptSpire__ep2_Epsigard__ep4_ShambertinPhase1 = 0x24,
+        ep1_VolOptForm2__ep2_DelLily__ep4_SpinnerShambertin1 = 0x25,
+        ep1_VolOptPrison__ep2_IllGill__ep4_ShambertinPhase2 = 0x26,
+        ep1_None__ep2_None__ep4_SpinnerShambertin2 = 0x27,
+        ep1_None__ep2_None__ep4_KondrieuPhase1 = 0x28,
+        ep1_None__ep2_None__ep4_SpinnerKondrieu1 = 0x29,
+        ep1_VolOptForm2__ep2_None__ep4_KondrieuPhase2 = 0x2a,
+        ep1_None__ep2_OlgaFlowForm1__ep4_SpinnerKondrieu2 = 0x2b,
+        ep1_None__ep2_OlgaFlowForm2__ep4_None = 0x2c,
+        ep1_None__ep2_Gael__ep4_None = 0x2d,
+        ep1_None__ep2_Giel__ep4_None = 0x2e,
+        ep1_PofuillySlime__ep2_Deldepth__ep4_None = 0x30,
+        ep1_PanArms__ep2_PanArms__ep4_None = 0x31,
+        ep1_Hidoom__ep2_Hidoom__ep4_None = 0x32,
+        ep1_Migium__ep2_Migium__ep4_None = 0x33,
+        ep1_PouillySlime__ep2_None__ep4_None = 0x34,
+        ep1_Darvant__ep2_None__ep4_None = 0x35,
+        ep1_DarkFalzForm1__ep2_None__ep4_None = 0x36,
+        ep1_DarkFalzForm2__ep2_None__ep4_None = 0x37,
+        ep1_DarkFalzForm3__ep2_None__ep4_None = 0x38,
+        ep1_DarvantFalz__ep2_None__ep4_None = 0x39,
+        ep1_None__ep2_Mericarol__ep4_None = 0x3a,
+        ep1_None__ep2_UlGibbon__ep4_None = 0x3b,
+        ep1_None__ep2_ZolGibbon__ep4_None = 0x3c,
+        ep1_None__ep2_Gibbles__ep4_None = 0x3d,
+        ep1_None__ep2_Morfos__ep4_None = 0x40,
+        ep1_None__ep2_Recobox__ep4_None = 0x41,
+        ep1_None__ep2_Recon__ep4_None = 0x42,
+        ep1_None__ep2_SinowZoa__ep4_None = 0x43,
+        ep1_None__ep2_SinowZele__ep4_None = 0x44,
+        ep1_None__ep2_Merikle__ep4_None = 0x45,
+        ep1_None__ep2_Mericus__ep4_None = 0x46,
+        ep1_Hildebear__ep2_Hildebear__ep4_None = 0x48,
+        ep1_Hildeblue__ep2_Hildeblue__ep4_None = 0x49,
+        ep1_Booma__ep2_Merillia__ep4_None = 0x4a,
+        ep1_Gobooma__ep2_Meriltas__ep4_None = 0x4b,
+        ep1_Gigobooma__ep2_None__ep4_None = 0x4c,
+        ep1_GrassAssassin__ep2_GrassAssassin__ep4_None = 0x4d,
+        ep1_EvilShark__ep2_Dolmolm__ep4_None = 0x4e,
+        ep1_PalShark__ep2_Dolmdarl__ep4_None = 0x4f,
+        ep1_GuilShark__ep2_None__ep4_None = 0x50,
+        ep1_Delsaber__ep2_Delsaber__ep4_None = 0x51,
+        ep1_Dimenian__ep2_Dimenian__ep4_None = 0x52,
+        ep1_LaDimenian__ep2_LaDimenian__ep4_None = 0x53,
+        ep1_SoDimenian__ep2_SoDimenian__ep4_None = 0x54,
+
+        None = 0xff
+    };
+
+    /// Highest used index
+    const size_t END_INDEX = 0x5c;
+
+    typedef union {
+        BPStatsIndex stats;
+        BPAttacksIndex attacks;
+        BPResistsIndex resists;
+        BPAnimationsIndex animations;
+    } BPIndex;
 
 #pragma pack(push, 1)
     typedef struct {
@@ -196,10 +468,10 @@ namespace BattleParam
     } BPResistsEntry;
 
     typedef struct {
-        BPStatsEntry stats[96];
-        BPAttacksEntry attacks[96];
-        BPResistsEntry resists[96];
-        BPAnimationsEntry animations[96];
+        BPStatsEntry stats[4][96];
+        BPAttacksEntry attacks[4][96];
+        BPResistsEntry resists[4][96];
+        BPAnimationsEntry animations[4][96];
     } BPFile;
 #pragma pack(pop)
 
@@ -212,5 +484,5 @@ namespace BattleParam
         Animations = 3
     };
 
-    void* GetBPEntry(Episode ep, bool solo_mode, BPIndex i, BPEntryType entryType);
+    void* GetBPEntry(Episode ep, bool solo_mode, uint8_t i, BPEntryType entryType);
 };
