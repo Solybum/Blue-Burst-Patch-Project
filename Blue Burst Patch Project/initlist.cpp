8848dae4a382fdda45b6066838df05b30a5c318a
jtuu <joona.tuukkanen@gmail.com>
2021-12-22 01:08:21 +0200

    Added support for null terminated initlists. Added a bunch of overloads to InitList methods. Init functions must be unique within an InitList. InitLists are only patched when they have been modified.

--- Blue Burst Patch Project/initlist.cpp
+++ Blue Burst Patch Project/initlist.cpp
@@ -4,59 +4,213 @@
 #include <sstream>
 #include "initlist.h"
 
-std::map<size_t, std::unique_ptr<InitList>> InitList::initLists;
+std::map<const InitList::FunctionPair*, std::unique_ptr<InitList>> InitList::initLists;
 bool InitList::patchApplied = false;
+const InitList::FunctionPair InitList::nullTerminator = FunctionPair(nullptr, nullptr);
 
 InitList::FunctionPair::FunctionPair(InitList::Function init_, InitList::Function uninit_) : init(init_), uninit(uninit_) {}
+
+InitList::FunctionPair::FunctionPair(size_t init_, size_t uninit_) : init(reinterpret_cast<InitList::Function>(init_)), uninit(reinterpret_cast<InitList::Function>(uninit_)) {}
+
 InitList::FunctionPair::FunctionPair() : init(nullptr), uninit(nullptr) {}
 
-InitList::InitList(FunctionPair* listStart, FunctionPair* listEnd) :
-    origList(listStart),
-    origPairCount((listEnd - listStart) / sizeof(FunctionPair))
+bool InitList::FunctionPair::operator<(const FunctionPair& other) const
 {
-    FunctionPair* current = listStart;
+    return ((size_t) init) < ((size_t) other.init);
+}
+
+InitList::InitList(const FunctionPair* listStart, const FunctionPair* listEnd) :
+    origList(listStart),
+    origPairCount((listEnd - listStart) / sizeof(FunctionPair)),
+    nullTerminated(false)
+{
+    const FunctionPair* current = listStart;
 
     // Read function pairs into vector
     while (current < listEnd)
     {
-        functionPairs.push_back(*current);
+        AddFunctionPair(*current);
         current++;
     }
 }
 
-InitList& InitList::GetInitList(size_t listStartAddr, size_t listEndAddr)
+InitList::InitList(const FunctionPair* listStart) :
+    origList(listStart),
+    nullTerminated(true)
 {
-    auto entry = initLists.find(listStartAddr);
+    const FunctionPair* current = listStart;
+
+    // Read function pairs into vector
+    while (current->init != nullptr)
+    {
+        AddFunctionPair(*current);
+        current++;
+    }
+
+    origPairCount = (current - listStart) / sizeof(FunctionPair);
+}
+
+InitList& InitList::GetInitList(const FunctionPair* listStart, const FunctionPair* listEnd)
+{
+    auto entry = initLists.find(listStart);
 
     if (entry == initLists.end())
     {
         // No existing object found, create new
-        initLists[listStartAddr] = std::unique_ptr<InitList>(
-            new InitList(reinterpret_cast<FunctionPair*>(listStartAddr),
-                         reinterpret_cast<FunctionPair*>(listEndAddr)));
+        initLists[listStart] = std::unique_ptr<InitList>(new InitList(listStart, listEnd));
     }
 
-    return *initLists[listStartAddr];
+    return *initLists[listStart];
 }
 
-void InitList::AddListReferenceAddress(std::initializer_list<size_t> addrs)
+InitList& InitList::GetInitList(size_t listStartAddr, size_t listEndAddr)
+{
+    return GetInitList(reinterpret_cast<const FunctionPair*>(listStartAddr), reinterpret_cast<const FunctionPair*>(listEndAddr));
+}
+
+InitList& InitList::GetNullTerminatedInitList(const FunctionPair* listStart)
+{
+    auto entry = initLists.find(listStart);
+
+    if (entry == initLists.end())
+    {
+        // No existing object found, create new
+        initLists[listStart] = std::unique_ptr<InitList>(new InitList(listStart));
+    }
+
+    return *initLists[listStart];
+}
+
+InitList& InitList::GetNullTerminatedInitList(size_t listStartAddr)
+{
+    return GetNullTerminatedInitList(reinterpret_cast<const FunctionPair*>(listStartAddr));
+}
+
+void InitList::AddListReferenceAddress(FunctionPair** addr)
+{
+    listReferenceAddresses.insert(addr);
+}
+
+void InitList::AddListReferenceAddress(size_t addr)
+{
+    AddListReferenceAddress(reinterpret_cast<FunctionPair**>(addr));
+}
+
+void InitList::AddListReferenceAddress(std::initializer_list<FunctionPair**> addrs)
 {
     listReferenceAddresses.insert(addrs);
 }
 
-void InitList::AddSizeReferenceAddress(std::initializer_list<size_t> addrs)
+void InitList::AddListReferenceAddress(std::initializer_list<size_t> addrs)
+{
+    for (size_t addr : addrs)
+    {
+        AddListReferenceAddress(reinterpret_cast<FunctionPair**>(addr));
+    }
+}
+
+void InitList::AddSizeReferenceAddress(SizeRefValueType* addr)
+{
+    sizeReferenceAddresses.insert(addr);
+}
+
+void InitList::AddSizeReferenceAddress(size_t addr)
+{
+    AddSizeReferenceAddress(reinterpret_cast<SizeRefValueType*>(addr));
+}
+
+void InitList::AddSizeReferenceAddress(std::initializer_list<SizeRefValueType*> addrs)
 {
     sizeReferenceAddresses.insert(addrs);
 }
 
+void InitList::AddSizeReferenceAddress(std::initializer_list<size_t> addrs)
+{
+    for (size_t addr : addrs)
+    {
+        AddSizeReferenceAddress(reinterpret_cast<SizeRefValueType*>(addr));
+    }
+}
+
 void InitList::AddFunctionPair(const FunctionPair& funcPair)
 {
+    // Check duplicate
+    for (const FunctionPair& existing : functionPairs)
+    {
+        if (existing.init == funcPair.init) return;
+    }
+
     functionPairs.push_back(funcPair);
 }
 
 void InitList::AddFunctionPair(const FunctionPair&& funcPair)
 {
-    functionPairs.push_back(funcPair);
+    AddFunctionPair(funcPair);
+}
+
+void InitList::SetNullTerminated(bool option)
+{
+    nullTerminated = option;
+}
+
+bool InitList::HasChanged()
+{
+    return functionPairs.size() != origPairCount;
+}
+
+void InitList::Patch()
+{
+    if (!HasChanged())
+    {
+        // Nothing to do
+        return;
+    }
+
+    // This whole thing won't do anything if there isn't at least one list reference
+    if (listReferenceAddresses.empty())
+    {
+        std::runtime_error(toString() + ": At least one list reference must be defined");
+    }
+
+    // Check initlist size
+    size_t pairCount = functionPairs.size();
+    if (pairCount > MAX_FN_PAIR_COUNT)
+    {
+        std::runtime_error(toString() + ": Too many function pairs (was: " +
+            std::to_string(pairCount) + ", max: " + std::to_string(MAX_FN_PAIR_COUNT) + ")");
+    }
+
+    // Rewrite initlist size references
+    size_t byteSize = pairCount * sizeof(FunctionPair);
+    for (SizeRefValueType* address : sizeReferenceAddresses)
+    {
+        *address = (SizeRefValueType) byteSize;
+    }
+
+    size_t newListLength = functionPairs.size();
+    if (nullTerminated)
+    {
+        // Add space for null terminator
+        newListLength += 1;
+    }
+
+    // Copy the new list to an array on the heap which will be deliberately leaked
+    // because we need the initlists to stay allocated for the entire lifetime of the program anyway
+    // and we'll just let the system clean them up after exiting
+    FunctionPair* listCopy = new FunctionPair[newListLength];
+    std::copy(functionPairs.begin(), functionPairs.end(), listCopy);
+
+    if (nullTerminated)
+    {
+        // Append null terminator
+        listCopy[functionPairs.size()] = InitList::nullTerminator;
+    }
+
+    // Rewrite initlist references
+    for (FunctionPair** address : listReferenceAddresses)
+    {
+        *address = listCopy;
+    }
 }
 
 void InitList::PatchAllInitLists()
@@ -70,40 +224,8 @@ void InitList::PatchAllInitLists()
 
     for (const auto& entry : initLists)
     {
-        InitList& newList = *entry.second;
-
-        // Check initlist size
-        size_t pairCount = newList.functionPairs.size();
-        if (pairCount > MAX_FN_PAIR_COUNT)
-        {
-            std::runtime_error(newList.toString() + ": Too many function pairs (was: " +
-                std::to_string(pairCount) + ", max: " + std::to_string(MAX_FN_PAIR_COUNT) + ")");
-        }
-
-        // Rewrite initlist size references
-        size_t byteSize = pairCount * sizeof(FunctionPair);
-        for (size_t address : newList.sizeReferenceAddresses)
-        {
-            *(uint8_t*) address = (uint8_t) byteSize;
-        }
-
-        // This whole thing won't do anything if there isn't at least one list reference
-        if (newList.listReferenceAddresses.empty())
-        {
-            std::runtime_error(newList.toString() + ": At least one list reference must be defined");
-        }
-
-        // Copy the new list to an array on the heap which will be deliberately leaked
-        // because we need the initlists to stay allocated for the entire lifetime of the program anyway
-        // and we'll just let the system clean them up after exiting
-        FunctionPair* listCopy = new FunctionPair[newList.functionPairs.size()];
-        std::copy(newList.functionPairs.begin(), newList.functionPairs.end(), listCopy);
-
-        // Rewrite initlist references
-        for (size_t address : newList.listReferenceAddresses)
-        {
-            *(FunctionPair**) address = listCopy;
-        }
+        InitList& lst = *entry.second;
+        lst.Patch();
     }
 
     patchApplied = true;
--- Blue Burst Patch Project/initlist.h
+++ Blue Burst Patch Project/initlist.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cstddef>
 #include <memory>
 #include <vector>
@@ -16,49 +18,75 @@ public:
     typedef void (__cdecl *Function)(void);
 
 #pragma pack(push, 1)
+    /// The components of an InitList are FunctionPairs.
+    /// The components of a FunctionPair are pointers to functions that take no arguments and return nothing.
+    /// The first function is called when resources should be created and the second function is called when those resources should be freed.
+    /// In most cases the second function is not required, but the first one is required.
     struct FunctionPair
     {
         Function init;
         Function uninit;
 
         FunctionPair(Function init, Function uninit);
+        FunctionPair(size_t init, size_t uninit);
         FunctionPair();
+        bool operator<(const FunctionPair& other) const;
     };
 #pragma pack(pop)
+
+    // The instruction operands we patch are often byte-sized, so that is the maximum size we can use. This could be made configurable if needed.
+    typedef uint8_t SizeRefValueType;
+    static const size_t MAX_FN_PAIR_COUNT = sizeof(SizeRefValueType) / sizeof(FunctionPair);
 
 private:
     friend void PSOBB();
 
-    static std::map<size_t, std::unique_ptr<InitList>> initLists;
+    static std::map<const FunctionPair*, std::unique_ptr<InitList>> initLists;
     static bool patchApplied;
+    static const FunctionPair nullTerminator;
 
-    FunctionPair* origList;
+    const FunctionPair* origList;
     size_t origPairCount;
-    std::set<size_t> listReferenceAddresses;
-    std::set<size_t> sizeReferenceAddresses;
+    std::set<FunctionPair**> listReferenceAddresses;
+    std::set<SizeRefValueType*> sizeReferenceAddresses;
+    // It is important to maintain the original ordering of the initlist
     std::vector<FunctionPair> functionPairs;
+    bool nullTerminated;
 
     static void PatchAllInitLists();
+    bool HasChanged();
+    void Patch();
 
-    InitList(FunctionPair* listStart, FunctionPair* listEnd);
+    InitList(const FunctionPair* listStart, const FunctionPair* listEnd);
+    InitList(const FunctionPair* listStart);
+
 
 public:
-    /// The total byte size of an initlist must fit within a byte
-    static const size_t MAX_FN_PAIR_COUNT = 0xff / sizeof(FunctionPair);
-
     /// Add addresses that reference the size of the initlist.
     /// These values will be rewritten to the new size of the initlist.
+    void AddListReferenceAddress(size_t addr);
+    void AddListReferenceAddress(FunctionPair** addr);
     void AddListReferenceAddress(std::initializer_list<size_t> addrs);
+    void AddListReferenceAddress(std::initializer_list<FunctionPair**> addrs);
 
     /// Add addresses that reference the initlist's address.
     /// These pointers will be rewritten to point to the new location of the initlist.
+    void AddSizeReferenceAddress(size_t addr);
+    void AddSizeReferenceAddress(SizeRefValueType* addr);
     void AddSizeReferenceAddress(std::initializer_list<size_t> addrs);
+    void AddSizeReferenceAddress(std::initializer_list<SizeRefValueType*> addrs);
 
+    /// The pair will not be added if the first function of the pair already exists in the list.
     void AddFunctionPair(const FunctionPair& funcPair);
     void AddFunctionPair(const FunctionPair&& funcPair);
 
-    // size_t because it's probably easier to use than casting to a pointer
+    /// Should terminate list with a null element?
+    void SetNullTerminated(bool);
+
     static InitList& GetInitList(size_t listStartAddr, size_t listEndAddr);
+    static InitList& GetInitList(const FunctionPair* listStart, const FunctionPair* listEnd);
+    static InitList& GetNullTerminatedInitList(size_t listStartAddr);
+    static InitList& GetNullTerminatedInitList(const FunctionPair* listStart);
 
     std::string toString();
 };
