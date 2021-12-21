cb2fc76070bc9538ec7db70ca6649b7a12048e7b
jtuu <joona.tuukkanen@gmail.com>
2021-12-21 20:30:50 +0200

    Use InitList helper in slow Gibbles patch

--- Blue Burst Patch Project/slow_gibbles.cpp
+++ Blue Burst Patch Project/slow_gibbles.cpp
@@ -2,6 +2,7 @@
 #include "pch.h"
 #include "helpers.h"
 #include "slow_gibbles.h"
+#include "initlist.h"
 
 float* gibblesJumpAnimSpeed = (float*) 0x009be1e8;
 
@@ -14,7 +15,8 @@ void __cdecl SlowGibblesFix()
 void ApplySlowGibblesFix()
 {
     // Append to a function table that is called on party join/create
-    *((void**) 0x00a0f630) = &SlowGibblesFix;
-    // Adjust size of function table
-    *((uint8_t*) 0x007bd159) = 0x58;
+    InitList& lst = InitList::GetInitList(0x00a0f5e0, 0x00a0f630);
+    lst.AddFunctionPair(InitList::FunctionPair(SlowGibblesFix, nullptr));
+    lst.AddSizeReferenceAddress({0x007bcb80 + 1, 0x007bd158 + 1, 0x007bd558 + 1});
+    lst.AddListReferenceAddress({0x007bcb82 + 1, 0x007bd15a + 1, 0x007bd55a + 1});
 }
