df1b38250456dd9e97785adb1ca54fffe0697441
jtuu <joona.tuukkanen@gmail.com>
2022-01-21 23:49:41 +0200

    Link assimp statically Updated assimp to 5.1.6

--- wine-build.sh
+++ wine-build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 src_dir="Blue Burst Patch Project"
+libs="User32.lib"
 
-docker run --rm -v "$(readlink -f .)":/bbpp -w /bbpp msvc:latest bash -c \
-"/opt/msvc/bin/x86/cl /nologo /LD /EHsc /MT /DCINTERFACE \
-    /I'$src_dir' /I'$src_dir'/newgfx /Iinclude \
-    '$src_dir'/*.cpp '$src_dir'/**/*.cpp $* \
-    /link /OUT:bbpp.dll /subsystem:console User32.lib lib/assimp-vc142-mt.lib &&
+if [[ "$*" == *"PATCH_NEWENEMY"* ]]; then
+    libs="$libs lib/assimp-vc142-mt.lib lib/zlibstatic.lib Advapi32.lib"
+fi
+
+cmd="/opt/msvc/bin/x86/cl /nologo /LD /EHsc /MD /DCINTERFACE \
+/I'$src_dir' /I'$src_dir'/newgfx /Iinclude \
+'$src_dir'/*.cpp '$src_dir'/**/*.cpp $* \
+/link /OUT:bbpp.dll /subsystem:console $libs && \
 chown $(id -u $USER):$(id -g $USER) bbpp.dll"
+
+echo "$cmd"
+
+docker run --rm -v "$(readlink -f .)":/bbpp -w /bbpp msvc:latest bash -c "$cmd"
