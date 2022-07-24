e67d9ab7c3f123d20d0f8c91c8cccc9b6707b77a
jtuu <joona.tuukkanen@gmail.com>
2022-07-24 23:26:16 +0300

    Cleaned up fastwarp patch

--- Blue Burst Patch Project/fastwarp.cpp
+++ Blue Burst Patch Project/fastwarp.cpp
@@ -8,116 +8,78 @@ This patch works by skipping the sleep portion of the render function
 during loading screens to allow the game to load as fast as possible.
 */
 
-#define LOGIN_CONNECTION_MANAGER 0x00a3ad08
-#define QUEST_STARTED 0x00a95624
+auto InnerRenderFunction = reinterpret_cast<void (__fastcall *)(BOOL)>(0x0083ae04);
+auto OuterRenderFunction = reinterpret_cast<void (__cdecl *)()>(0x0080030c);
+auto SleepLoopFunction = reinterpret_cast<void (__cdecl *)()>(0x0082c43c);
 
-int sleepSkipAddr = 0x0083ae4c;
-int renderSkipInAddr = 0x0083ae11;
-int renderSkipOutAddr = 0x0083ae16;
+auto skipFrame = reinterpret_cast<BOOL*>(0x00aca200);
+auto loginManager = reinterpret_cast<void**>(0x00a3ad08);
+auto isInQuest = reinterpret_cast<BOOL*>(0x00a95624);
+auto isQuestLoading = reinterpret_cast<BOOL*>(0x00aab378);
+bool skipNextFrameAlso = false;
 
-int skipNextFrameAlso = 0;
-int skipSleep = 0;
-
-void __declspec(naked) CheckRenderSkip()
+bool IsLoginLoadingScreenActive()
 {
-    __asm
-    {
-        mov eax, dword ptr [skipSleep]
-        test eax, eax
-        jne goto_skip
+    return *loginManager != nullptr;
+}
 
-        // To skip the login loading screen, check if the login connection manager exists
-        mov eax, ds:[LOGIN_CONNECTION_MANAGER]
-        test eax, eax
+void __fastcall BeforeInnerRenderCall(BOOL shouldPresent)
+{
+    if (IsLoginLoadingScreenActive())
+    {
+        *skipFrame = true;
+        shouldPresent = false;
         // Also skip the next frame because the loading screen is up for one frame after login finishes
         // and showing only one frame of it looks ugly
-        jne skip_next_frame_also
-
-        mov eax, dword ptr [skipNextFrameAlso]
-        test eax, eax
-        // Don't skip the next frame anymore
-        jne dont_skip_next_frame
-
-    dont_skip:
-        jmp renderSkipOutAddr
-
-    skip_next_frame_also:
-        mov dword ptr [skipNextFrameAlso], 1
-        jmp goto_skip
-
-    dont_skip_next_frame:
-        mov dword ptr [skipNextFrameAlso], 0
-        jmp goto_skip
-
-    goto_skip:
-        jmp sleepSkipAddr
+        skipNextFrameAlso = true;
     }
+    else if (skipNextFrameAlso)
+    {
+        *skipFrame = true;
+        shouldPresent = false;
+        skipNextFrameAlso = false;
+    }
+
+    // Call the original function
+    InnerRenderFunction(shouldPresent);
 }
 
-int renderFuncAddr = 0x0080030c;
-
-int assetLoadPatchInAddr = 0x005b92d9;
-int assetLoadPatchOutAddr = 0x005b92de;
-
-int questNotLoadingFuncAddr = 0x007a6154;
-int questDoneLoadingInAddr = 0x007bcff4;
-int questDoneLoadingOutAddr = 0x007bcff9;
-
-int questDoneLoading = 0;
-
-void __declspec(naked) PatchAllAssetLoading()
+void __cdecl BeforeAssetLoadingRenderCall()
 {
-    __asm
-    {
+    // Setting skipFrame here will also cause the shouldPresent parameter for the inner render function to be false
 #ifdef FASTWARP_NO_QUEST
-        // Skip if quest not started
-        mov eax, ds:[QUEST_STARTED]
-        test eax, eax
-        je do_skip
-
-        // Don't skip if quest has finished loading
-        mov eax, dword ptr [questDoneLoading]
-        test eax, eax
-        jne dont_skip
+    // During a quest, the quest loading screen will be skipped but not any other loading screens
+    *skipFrame = !*isInQuest || *isQuestLoading;
+#else
+    *skipFrame = true;
 #endif
 
-    do_skip:
-        // Quest has not finished loading (or hasn't started loading)
-        mov dword ptr [questDoneLoading], 0
-        // To skip most other loading screens, check if the render function is being called from a certain asset loading function
-        mov dword ptr [skipSleep], 1
-    dont_skip:
-        call renderFuncAddr
-        mov dword ptr [skipSleep], 0
-        jmp assetLoadPatchOutAddr
-    }
+    // Call the original function
+    OuterRenderFunction();
 }
 
-void __declspec(naked) PatchInQuest()
+void __cdecl BeforeSleepLoopCall()
 {
-    __asm
+    if (*skipFrame)
     {
-        mov dword ptr [questDoneLoading], 1
-        call questNotLoadingFuncAddr
-        jmp questDoneLoadingOutAddr
+        *skipFrame = false;
+        return;
     }
-}
 
-#undef LOGIN_CONNECTION_MANAGER
-#undef QUEST_STARTED
+    // Call the original function
+    SleepLoopFunction();
+}
 
 void ApplyFastWarpPatch()
 {
-    // Replaces a useless call to a nop function
-    PatchJMP(renderSkipInAddr, renderSkipOutAddr, (int) &CheckRenderSkip);
+    // Handles skipping the login loading screen
+    PatchCALL(0x00800347, 0x0080034c, (int) BeforeInnerRenderCall);
 
-    // Makes asset loading fast
-    PatchJMP(assetLoadPatchInAddr, assetLoadPatchOutAddr, (int) &PatchAllAssetLoading);
+    // Handles skipping most other loading screens such as warping between areas and loading a quest
+    PatchCALL(0x005b92d9, 0x005b92de, (int) BeforeAssetLoadingRenderCall);
 
-#ifdef FASTWARP_NO_QUEST
-    // Sets a variable when quest loading has finished
-    PatchJMP(questDoneLoadingInAddr, questDoneLoadingOutAddr, (int) &PatchInQuest);
-#endif
+    // Handles skipping the sleep loop within the render function
+    PatchCALL(0x0083ae47, 0x0083ae4c, (int) BeforeSleepLoopCall);
 }
 
 #endif // PATCH_FASTWARP
