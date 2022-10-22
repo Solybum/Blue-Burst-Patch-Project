f5f167115747f8e1f6ec982d71e6bfd42132b340
jtuu <joona.tuukkanen@gmail.com>
2022-10-22 23:24:55 +0300

    CMake build error fixes

--- Blue Burst Patch Project/globals.h
+++ Blue Burst Patch Project/globals.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <Windows.h>
-static const WCHAR* clientName = L"psobb.exe";
+static const wchar_t* clientName = L"psobb.exe";
 
 // Main GameGuard call, nop 5 bytes to disable
 static const int addrMainGameGuardCall = 0x0082D315;
--- Blue Burst Patch Project/psobb.cpp
+++ Blue Burst Patch Project/psobb.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <stdint.h>
 #include "globals.h"
 
