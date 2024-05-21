36b84502c02096eb7bc4c55ca8f42bae8439e262
jtuu <joona.tuukkanen@gmail.com>
2024-05-21 17:45:16 +0300

    Added a few ninja render functions

--- Blue Burst Patch Project/ninja.cpp
+++ Blue Burst Patch Project/ninja.cpp
@@ -2,9 +2,13 @@
 
 decltype(UseTextureArchive) UseTextureArchive = reinterpret_cast<decltype(UseTextureArchive)>(0x00829cc8);
 decltype(RenderXj) RenderXj = reinterpret_cast<decltype(RenderXj)>(0x00835090);
+decltype(RenderAnimatedXjUp) RenderAnimatedXjUp = reinterpret_cast<decltype(RenderAnimatedXjUp)>(0x0083bd00);
 decltype(LoadBml) LoadBml = reinterpret_cast<decltype(LoadBml)>(0x005b7a84);
 decltype(FreeBml) FreeBml = reinterpret_cast<decltype(FreeBml)>(0x004066a8);
 decltype(LoadXvm) LoadXvm = reinterpret_cast<decltype(LoadXvm)>(0x008009dc);
 decltype(FreeXvm) FreeXvm = reinterpret_cast<decltype(FreeXvm)>(0x00829de0);
 decltype(PrepareBmlMeshes) PrepareBmlMeshes = reinterpret_cast<decltype(PrepareBmlMeshes)>(0x005b90d0);
 decltype(GetModel) GetModel = reinterpret_cast<decltype(GetModel)>(0x005b8610);
+decltype(GetMotion) GetMotion = reinterpret_cast<decltype(GetMotion)>(0x005b8638);
+decltype(DisableFog) DisableFog = reinterpret_cast<decltype(DisableFog)>(0x0082dbac);
+decltype(EnableFog) EnableFog = reinterpret_cast<decltype(EnableFog)>(0x0082dab0);
--- Blue Burst Patch Project/ninja.h
+++ Blue Burst Patch Project/ninja.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include "object_extension.h"
 
 #pragma pack(push, 1)
 struct BmlMetadata
@@ -11,13 +12,38 @@ struct BmlMetadata
     const uint16_t modelCount;
     const uint16_t animationCount;
 };
+
+struct TBinModel
+{
+    union
+    {
+        DEFINE_FIELD(0x42c, void* njcmChunks);
+        DEFINE_FIELD(0x430, void* njmChunks);
+        DEFINE_FIELD(0x434, uint32_t njcmCount);
+        DEFINE_FIELD(0x438, uint32_t njmCount);
+
+        uint8_t _padding[0x440];
+    };
+};
+
+struct NjmMotion
+{
+    void* nodesKeyframes;
+    uint32_t frameCount;
+    uint16_t motionFlags;
+    uint16_t factorCount;
+};
 #pragma pack(pop)
 
 extern void (__thiscall *UseTextureArchive)(void* tex);
 extern void (__fastcall *RenderXj)(void* model);
-extern void* (__thiscall *LoadBml)(void* result, uint32_t unk1, BmlMetadata* bmlMeta, void* unk2, uint32_t unk3, void* optionalDstBuf);
-extern void (__thiscall *FreeBml)(void* bml, uint32_t doFree);
+extern void (__fastcall *RenderAnimatedXjUp)(void* model, void* motion, float elapsed);
+extern TBinModel* (__thiscall *LoadBml)(TBinModel* result, uint32_t unk1, BmlMetadata* bmlMeta, void* unk2, uint32_t unk3, void* optionalDstBuf);
+extern void (__thiscall *FreeBml)(TBinModel* bml, uint32_t doFree);
 extern void* (__cdecl *LoadXvm)(const char* filename);
 extern uint32_t (__fastcall *FreeXvm)(void* xvm);
-extern void (__thiscall *PrepareBmlMeshes)(void* bml);
-extern void* (__thiscall *GetModel)(void* bml, uint32_t index);
+extern void (__thiscall *PrepareBmlMeshes)(TBinModel* bml);
+extern void* (__thiscall *GetModel)(TBinModel* bml, uint32_t index);
+extern NjmMotion* (__thiscall *GetMotion)(TBinModel* bml, uint32_t index);
+extern void (*DisableFog)();
+extern void (*EnableFog)();
